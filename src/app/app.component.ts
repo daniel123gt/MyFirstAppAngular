61d667cf00247d2f988d1b3b4f85262fdc7321aa
daniel123gt <42387658+daniel123gt@users.noreply.github.com>
2020-09-28 12:13:37 -0500

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-primera-angular';
+  title = 'Aplicación Angular';
+  personas: String[] = ["Carlos", "July", "Victor", "Alex"];
+  valido: boolean = true
 }
